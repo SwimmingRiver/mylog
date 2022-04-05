74e40864191acd91cfa224bf75dcb9e3c0c8aa96
River <swimmingr@gmail.com>
2022-04-06 00:17:10 +0900

    todoList update

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,5 +1,6 @@
 import  styled  from 'styled-components';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Wrapper = styled.div`
     text-align: center;
@@ -21,54 +22,60 @@ const ListBox = styled.div`
     height: 80px;
 `;
 const List = styled.li`
-    list-style: none;
+    list-style: none; 
 `;
 
 function ToDoList(){
-    const [todo,setTodo]=useState({
-        todo:"",
-        index:0,
-        state:"",
-    });
-    const [todoArr,setTodoArr]=useState([]);
-    const Add = (e)=>{
-        e.preventDefault();
-       if(todo===""){ 
-           return;
-        }
-       setTodoArr((item,index)=>[todo, ...{
-           todo:item,
-           index:index,
-           state:"to do",
-       }]);
-       setTodo("");
-       console.log(todoArr)
-    }
-    const Input =(e)=>{
-        let number = 0;
-        number++;
-        setTodo(
-            {
-                todo:e.target.value,
-                index:number,
-                state:"to do",
-            }
-        );
-        console.log(e.target.value)
-        
-    }
-    return(
-      <Wrapper>
-      <h1>ToDoList</h1>
+    const [todo, setTodo] = useState("");
+    //const [done,setDone]=useState(false);
+  let todoObj = {
+    todo: todo,
+    index: Number,
+    done: false
+  };
+  const [todoArr, setTodoArr] = useState([]);
+  const Typing = (e) => {
+    setTodo(e.target.value);
+  };
+  const Add = (e) => {
+    e.preventDefault();
+    if(todo === ""){return}
+    setTodoArr((i) => [todoObj.todo, ...i]);
+    setTodo("") 
+    console.log(todoObj);
+  };
+  const DoOrDone = (i,e) => {
+   let arr = [];
+   arr[i]=[e];
+   if(e===true){e=false}
+    else{e=true}
+    console.log(i,e);
+  };
+  const Undo = (arr,i )=> {
+    arr.splice(i,1);
+    setTodoArr((i)=>[...arr]);
+  };// 식제는 되는데 업데이트가 바로 안됨
+  const Edit=()=>{return "edit"}
+  return (
+    <>
+      <h1>TO DO LIST({todoArr.length})</h1>
       <form onSubmit={Add}>
-          <input  onChange={Input} value={todo.todo} type='text' placeholder="TO DO"/>
-          <button>add</button>
+        <input onChange={Typing} placeholder="To Do" type="text" value={todo} />
+        <button>Add</button>
       </form>
       <ul>
-          {todoArr.map((i,num)=>(<li key={num}>{todoArr[num]}<button>check</button></li>))}
+        To Do <hr/>
+        {todoArr.map((item, index) => (
+          <li key={(todoObj.index = index)}>
+            {todoObj.done ? "done!" : item}
+            <button onClick={()=>DoOrDone(todoObj.index,todoObj.done)}>Done</button>
+            <button onClick={()=>Undo(todoArr,index)}>Undo</button>
+            <button onClick={Edit}>Edit</button>
+          </li>
+        ))}
       </ul>
-      </Wrapper>
-    )
+    </>
+  );
 }
 
 export default ToDoList;
