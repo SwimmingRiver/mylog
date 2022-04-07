b754a03c6d5e8792f0f0d241af060e66668c8a87
River <swimmingr@gmail.com>
2022-04-08 01:07:54 +0900

    todoList Update

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,6 +1,5 @@
 import  styled  from 'styled-components';
 import { useState } from 'react';
-import { useEffect } from 'react';
 
 const Wrapper = styled.div`
     text-align: center;
@@ -11,8 +10,6 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
     text-align: center;
-    background-color: limegreen;
-    color: whitesmoke;
     height: 100px;
     font-size: 50px;
 `;
@@ -24,16 +21,19 @@ const ListBox = styled.div`
 const List = styled.li`
     list-style: none; 
 `;
+const DoneList = styled.li`
+  text-decoration: line-through;
+  font-style: italic;
+`;
 
 function ToDoList(){
     const [todo, setTodo] = useState("");
-    //const [done,setDone]=useState(false);
   let todoObj = {
     todo: todo,
-    index: Number,
-    done: false
+    index: 0,
   };
   const [todoArr, setTodoArr] = useState([]);
+  const [doneArr,setDoneArr]=useState([]); 
   const Typing = (e) => {
     setTodo(e.target.value);
   };
@@ -41,40 +41,60 @@ function ToDoList(){
     e.preventDefault();
     if(todo === ""){return}
     setTodoArr((i) => [todoObj.todo, ...i]);
-    setTodo("") 
-    console.log(todoObj);
+    // console.log(todoObj);
+    setTodo("") ;
   };
-  const DoOrDone = (i,e) => {
-   let arr = [];
-   arr[i]=[e];
-   if(e===true){e=false}
-    else{e=true}
-    console.log(i,e);
-  };
+
+
+  const DoOrDone = (arr,i) => {
+  
+    if(window.confirm("Is it done?")){
+    let gotodone = arr.splice(i,1);
+    setDoneArr((e)=>[gotodone,...e]);
+    }
+    
+  };//완료
+
   const Undo = (arr,i )=> {
-    arr.splice(i,1);
+    if(window.confirm("Is it undo?")){
+      arr.splice(i,1);
     setTodoArr((i)=>[...arr]);
-  };// 식제는 되는데 업데이트가 바로 안됨
-  const Edit=()=>{return "edit"}
+  }
+  };//삭제
+
+  const Edit=(arr,i)=>{
+    let editedTodo = prompt("edit","");
+    if(editedTodo === null){return null}
+    else{
+      arr.splice(i,1,editedTodo)
+      setTodoArr(()=>[...arr])
+    }
+  }//수정
   return (
-    <>
-      <h1>TO DO LIST({todoArr.length})</h1>
+    <Wrapper>
+      <Title>TO DO LIST</Title>
       <form onSubmit={Add}>
         <input onChange={Typing} placeholder="To Do" type="text" value={todo} />
         <button>Add</button>
       </form>
       <ul>
-        To Do <hr/>
+        To Do({todoArr.length}) <hr/>
         {todoArr.map((item, index) => (
-          <li key={(todoObj.index = index)}>
-            {todoObj.done ? "done!" : item}
-            <button onClick={()=>DoOrDone(todoObj.index,todoObj.done)}>Done</button>
+          <List key={(todoObj.index = index)}>
+            {item}
+            <button onClick={()=>DoOrDone(todoArr,index)}>Done</button>
             <button onClick={()=>Undo(todoArr,index)}>Undo</button>
-            <button onClick={Edit}>Edit</button>
-          </li>
+            <button onClick={()=>Edit(todoArr,index)}>Edit</button>
+          </List>
         ))}
       </ul>
-    </>
+      <ul>
+        Done({doneArr.length}) <hr/>
+        {doneArr.map((item,index)=>(
+          <DoneList key={index}>{item}</DoneList>
+        ))}
+      </ul>
+    </Wrapper>
   );
 }
 
