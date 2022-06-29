9330e218c5eaff4f475d48107a5a692c7df5806d
River <swimmingr@gmail.com>
2022-06-29 18:52:06 +0900

    220629

--- src/Routers/Main.js
+++ src/Routers/Main.js
@@ -7,6 +7,11 @@ function Main(){
         <p>연습용 미니 프로젝트들 모음</p>
         <br/>
         <p>아직 미완 계속 추가예정</p>
+        <span class="material-symbols-outlined" style={{
+            fontSize:"10em"
+            }}>
+            construction
+         </span>
         </>
         )
 }
